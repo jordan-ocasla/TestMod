d5d22cc6cf91b79037d289cb77ddd3c160863931
Jordan Ocasla <jbocasla@icloud.com>
2025-02-26 14:02:30 -0800

    Added Redlib Library and updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,20 @@
-#include <iostream>
+#include <RED4ext/RED4ext.hpp>
+// You must include RedLib. Note that it introduces an alias
+// such as namespace RED4ext can be replaced by Red.
+#include <RedLib.hpp>
 
-using namespace std;
-
-int main()
-{
-
-    cout << "Hello World!" << endl;
-    return 0;
+RED4EXT_C_EXPORT bool RED4EXT_CALL Main(RED4ext::PluginHandle aHandle,
+                                        RED4ext::EMainReason aReason,
+                                        const RED4ext::Sdk* aSdk) {
+  switch (aReason)  {
+  case RED4ext::EMainReason::Load: {
+    // It will automatically register types declared below.
+    Red::TypeInfoRegistrar::RegisterDiscovered();
+    break;
+  }
+  case RED4ext::EMainReason::Unload: {
+    break;
+  }
+  }
+  return true;
 }
